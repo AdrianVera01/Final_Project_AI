9e9a3cca907005ceb88394b5ff22758de7b76793
Adrian Vera <adrianvera8600@gmail.com>
2022-05-11 18:51:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import numpy as np 
+import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn import decomposition
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA, KernelPCA
+from sklearn.model_selection import train_test_split
+from matplotlib.colors import ListedColormap
+from sklearn import svm
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import matthews_corrcoef
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_curve,roc_auc_score
+
+
 print("Hello World")
